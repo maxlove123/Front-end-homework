c1edc949c591f3c2d2e0def58230bc51fc9ed9ed
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2017-12-18 19:46:44 +0800

    jd grid-1

--- jd/js/index.js
+++ jd/js/index.js
@@ -1,17 +1,20 @@
-var SlideShow = function() {
-    this.imgQueue = document.querySelectorAll('.tbanner li');
-    this.pointQueue = document.querySelectorAll('.indexbox .index');
+var SlideShow = function(slideBox, pointBox) {
+    this.imgQueue = slideBox.children;
+    this.pointQueue = pointBox.children;
     this.isRun = true;
     this.length = this.imgQueue.length;
     this.index = 0;
+    this.pointColor = this.pointQueue[0].style.background;
 };
+
 
 SlideShow.prototype.clean = function() {
     for (var i = 0; i < this.length; i++) {
         var node = this.imgQueue[i];
         node.style.zIndex = '0';
         node.classList.remove('fadein');
-        this.pointQueue[i].style.background = 'white';
+        this.pointQueue[i].style.background = this.pointColor;
+
     }
 };
 
@@ -32,7 +35,7 @@ SlideShow.prototype.setFocus = function(index) {
     this.clean();
     this.setLastFocusTag(this.imgQueue[this.index]);
 
-    if (index) {
+    if (index !== undefined) {
         this.index = index;
     } else if (this.index >= this.length - 1) {
         this.index = 0;
@@ -55,10 +58,15 @@ SlideShow.prototype.init = function() {
     ].join('');
     document.head.appendChild(style);
 
+    var order = 0;
+    for (var i = 0; i < this.pointQueue.length; i++) {
+        this.pointQueue[i].order = order++;
+    }
+
     this.imgQueue[0].style.zIndex = '2';
     this.pointQueue[0].style.background = '#f10215';
     var that = this;
-    var box = document.querySelector('.tbanner');
+    var box = this.imgQueue[0].parentNode.parentNode;
     box.addEventListener('mouseover', function() {
         that.isRun = false;
     });
@@ -69,8 +77,7 @@ SlideShow.prototype.init = function() {
     for (var j = 0; j < this.length; j++) {
         var pointNode = this.pointQueue[j];
         pointNode.addEventListener('mouseover', function() {
-            var index = this.getAttribute('data-order');
-            that.setFocus(index);
+            that.setFocus(this.order);
         });
     }
 };
@@ -99,8 +106,20 @@ var seckillTimer = function() {
 };
 
 var init = function() {
-    var s = new SlideShow();
+    var slideBox = document.querySelector('.tbanner .sildeBox');
+    var pointBox = document.querySelector('.tbanner .pointBox');
+    var s = new SlideShow(slideBox, pointBox);
     s.run();
+
+    var slideBox = document.querySelector('.seckillb .slideBox');
+    var pointBox = document.querySelector('.seckillb .pointBox');
+    var ls = new SlideShow(slideBox, pointBox);
+    ls.run();
+
+    var slideBox = document.querySelector('.grid-1 .ord-2 .slideBox');
+    var pointBox = document.querySelector('.grid-1 .ord-2 .dotBox');
+    var ls = new SlideShow(slideBox, pointBox);
+    ls.run();
 
     seckillTimer();
     var noticeBar = document.getElementById('noticebar');
